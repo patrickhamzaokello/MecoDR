46d2d85e246ce0877b508269f1804a6d42e2ab02
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2022-11-17 20:17:05 +0300

    update on final predict code

--- model.py
+++ model.py
@@ -3,63 +3,40 @@ import pandas as pd
 import os
 import gc
 from sklearn.model_selection import train_test_split
+
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers
-import tensorflow_addons as tfa
+from tensorflow.keras.models import load_model
 
 # Display
-from IPython.display import Image, display
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
-from PIL import Image
-
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
-# second cell
-TRAIN_PATH = '../input/aptos2019-blindness-detection/train_images/'
-DF_TRAIN = pd.read_csv('../input/aptos2019-blindness-detection/train.csv', dtype='str')
-DF_TRAIN['image_path'] = TRAIN_PATH + DF_TRAIN["id_code"] + ".png"
-DF_TRAIN.head()
+trained_checkpoint = '../input/dr-model-checks/checkpoint'
+sample_image_path = '../input/sample-image/cd563556cb57.png'
 
-# class lables
-classes = {0 : "No DR",
-           1 : "Mild",
-           2 : "Moderate",
-           3 : "Severe",
-           4 : "Proliferative"}
+classes = {0: "No DR",
+           1: "Mild",
+           2: "Moderate",
+           3: "Severe",
+           4: "Proliferative"}
 
-# make reshapre dir
-
-for img in DF_TRAIN['image_path']:
-    img_outfpath = "./train_imgs_reshaped/" + img.split('/')[-1]
-    image = Image.open(img)
-    image = image.resize((512,512),Image.ANTIALIAS)
-    image.save(fp=img_outfpath)
-
-# trains dir
-TRAIN_PATH_RS = './train_imgs_reshaped/'
-DF_TRAIN_RS = pd.read_csv('../input/aptos2019-blindness-detection/train.csv', dtype='str')
-DF_TRAIN_RS['image_path'] = TRAIN_PATH_RS + DF_TRAIN_RS["id_code"] + ".png"
-DF_TRAIN_RS.head()
-
-
-# show demo image
 from PIL import Image
-image_path = Image.open('./train_imgs_reshaped/000c1434d8d7.png')
+
+image_path = Image.open(sample_image_path)
 plt.imshow(image_path)
 plt.axis("off")
 
-# variables
 num_classes = 5
 input_shape = (512, 512, 3)
-learning_rate = 1e-4 #0.001
+learning_rate = 1e-4  # 0.001
 weight_decay = 0.0001
-batch_size = 16 #256
+batch_size = 16  # 256
 num_epochs = 2
 # We'll resize input images to this size
-image_size =  256
+image_size = 256
 # Size of the patches to be extract from the input images
 patch_size = 7
 num_patches = (image_size // patch_size) ** 2
@@ -72,269 +49,20 @@ transformer_units = [
 ]
 transformer_layers = 8
 # Size of the dense layers of the final classifier
-mlp_head_units = [56, 28] #[1024, 512]
-# define the path to the serialized output model after training
-MODEL_PATH = "dr_detector.model"
+mlp_head_units = [56, 28]  # [1024, 512]
 
-# load function
-@tf.function
-def load(image_file, target):
-    image = tf.io.read_file(image_file)
-    image = tf.image.decode_png(image)
+# load
 
-    image_ = tf.cast(image, tf.uint8)
-    return image, target
+print("[INFO] loading model...")
 
-# loaders
-train_loader = (
-    tf.data.Dataset
-    .from_tensor_slices((X_train,y_train))
-    .map(load, num_parallel_calls=AUTOTUNE)
-    .shuffle(7)
-    .batch(batch_size)
-)
-test_loader = (
-    tf.data.Dataset
-    .from_tensor_slices((X_test,y_test))
-    .map(load, num_parallel_calls=AUTOTUNE)
-    .shuffle(7)
-    .batch(batch_size)
-)
-
-# train batch
-train_batch = (
-    tf.data.Dataset
-    .from_tensor_slices((X_train,y_train))
-    .map(load, num_parallel_calls=AUTOTUNE)
-    .shuffle(7)
-    .batch(X_train.shape[0]-100)#X_train.shape[0]-100
-)
-#next(iter(train_batch))[0].shape
-
-# data augmentation
-data_augmentation = keras.Sequential(
-    [
-        layers.experimental.preprocessing.Normalization(),
-        layers.experimental.preprocessing.Resizing(image_size, image_size),
-        layers.experimental.preprocessing.RandomFlip("horizontal"),
-        layers.experimental.preprocessing.RandomRotation(factor=0.02),
-        layers.experimental.preprocessing.RandomZoom(
-            height_factor = 0.2, width_factor = 0.2
-        ),
-    ],
-     name="data_augmentation",
-)
-# Compute the mean and the variance of the training data for normalization.
-CompleteBatchData  =next(iter(train_batch))[0]
-data_augmentation.layers[0].adapt(CompleteBatchData)
-
-del CompleteBatchData
-gc.collect()
-
-
-#  Multi layer perceptron
-def mlp(x, hidden_units, dropout_rate):
-    for units in hidden_units:
-        x = layers.Dense(units, activation = tf.nn.gelu)(x)
-        x = layers.Dropout(dropout_rate)(x)
-    return x
-
-class Patches(layers.Layer):
-    def __init__(self, patch_size):
-        super(Patches, self).__init__()
-        self.patch_size = patch_size
-
-    def call(self, images):
-        batch_size = tf.shape(images)[0]
-        patches = tf.image.extract_patches(
-            images=images,
-            sizes=[1, self.patch_size, self.patch_size, 1],
-            strides=[1, self.patch_size, self.patch_size, 1],
-            rates=[1, 1, 1, 1],
-            padding="VALID",
-        )
-        patch_dims = patches.shape[-1]
-        # print(patches.shape)
-        patches = tf.reshape(patches, [batch_size, -1, patch_dims])
-        return patches
-
-    # pk added
-
-    def get_config(self):
-        config = super().get_config().copy()
-        config.update({
-            'patch_size': self.patch_size,
-        })
-        return config
-
-
-# show demo image with patches
-plt.figure(figsize=(8, 8))
-image = next(iter(train_loader))[0][5]
-
-plt.imshow(image)
-plt.axis("off")
-
-resized_image = tf.image.resize(
-    tf.convert_to_tensor([image]), size=(image_size, image_size)
-)
-print(resized_image.shape)
-patches = Patches(patch_size)(resized_image)
-print(f"Image size: {image_size} X {image_size}")
-print(f"Patch size: {patch_size} X {patch_size}")
-print(f"Patches per image: {patches.shape[1]}")
-print(f"Elements per patch: {patches.shape[-1]}")
-
-n = int(np.sqrt(patches.shape[1]))
-#print(n)
-
-plt.figure(figsize=(8, 8))
-for i, patch in enumerate(patches[0]):
-    ax = plt.subplot(n, n, i + 1)
-    patch_img = tf.reshape(patch, (patch_size, patch_size, 3))
-    plt.imshow(patch_img.numpy().astype('uint8'))
-    plt.axis("off")
-
-# Patch Encoder Class
-class PatchEncoder(layers.Layer):
-    def __init__(self, num_of_patches, projection_dim):
-        super(PatchEncoder, self).__init__()
-        self.num_patches = num_patches
-        self.projection = layers.Dense(units=projection_dim)
-        self.position_embedding = layers.Embedding(
-            input_dim=num_patches, output_dim=projection_dim
-        )
-
-    def call(self, patch):
-        positions = tf.range(start=0, limit=self.num_patches, delta=1)
-        encode = self.projection(patch) + self.position_embedding(positions)
-        return encode
-
-    # pk added 2
-
-    def get_config(self):
-        config = super().get_config().copy()
-        config.update({
-            'num_patches': self.num_patches,
-            'projection': self.projection,
-            'position_embedding': self.position_embedding,
-
-        })
-        return config
-
-# model architecture
-def vit_model():
-    inputs = layers.Input(shape=input_shape)
-    # Augment data.
-    augmented = data_augmentation(inputs)
-    # Create patches.
-    patches = Patches(patch_size)(augmented)
-    # Encode patches.
-    encoded_patches = PatchEncoder(num_patches, projection_dim)(patches)
-
-    # Create multiple layers of the Transformer block.
-    for _ in range(transformer_layers):
-        # Layer normalization 1.
-        x1 = layers.BatchNormalization()(encoded_patches)
-        # create a multi-head attention layer
-        attention_output = layers.MultiHeadAttention(
-            num_heads=num_heads, key_dim=projection_dim, dropout=0.1
-        )(x1, x1)
-        # Skip connection 1.
-        x2 = layers.Add()([attention_output, encoded_patches])
-        # Layer normalization 2.
-        x3 = layers.BatchNormalization()(x2)
-        # MLP.
-        x3 = mlp(x3, hidden_units=transformer_units, dropout_rate=0.1)
-        # Skip connection 2.
-        encoded_patches = layers.Add()([x3, x2])
-
-    # Create a [batch_size, projection_dim] tensor.
-    representation = layers.LayerNormalization()(encoded_patches)
-    representation = layers.Flatten()(representation)
-    representation = layers.Dropout(0.5)(representation)
-    # Add MLP
-    features = mlp(representation, hidden_units=mlp_head_units, dropout_rate=0.5)
-    # Classify outputs.
-    logits = layers.Dense(num_classes, activation='softmax')(features)
-    # create keras model
-    model = keras.Model(inputs=inputs, outputs=logits)
-    return model
-
-# experiment faction
-def experiment(model):
-    optimizer = tfa.optimizers.AdamW(
-        learning_rate=learning_rate, weight_decay=weight_decay
-    )
-
-    model.compile(
-        optimizer=optimizer,
-        loss=keras.losses.CategoricalCrossentropy(from_logits=True),
-        metrics=[
-            keras.metrics.CategoricalAccuracy(name="accuracy"),
-            keras.metrics.AUC(name="AUC"),
-        ],
-    )
-    checkpoint_filepath = "./tmp/checkpoint"
-    checkpoint_callback = keras.callbacks.ModelCheckpoint(
-        checkpoint_filepath,
-        monitor="val_accuracy",
-        save_best_only=True,
-        save_weights_only=True,
-    )
-
-    history = model.fit(train_loader,
-                        batch_size=batch_size,
-                        epochs=num_epochs,
-                        validation_data=test_loader,
-                        callbacks=[checkpoint_callback], )
-    model.load_weights(checkpoint_filepath)
-    _, accuracy, auc = model.evaluate(test_loader)
-    print(f"Test accuracy: {round(accuracy * 100, 2)}%")
-    print(f"Test AUC: {round(auc * 100, 2)}%")
-
-    # serialize the model to disk
-    print("[INFO] saving model...")
-    #     model.save(MODEL_PATH, save_format="h5")
-    model.save(MODEL_PATH)
-
-    return history
-
+vit_classifier = load_model("../input/trained-model")
+vit_classifier.load_weights(trained_checkpoint)
 
 # model summary
-vit_classifier = vit_model()
 vit_classifier.summary()
 
-# train model
-history = experiment(vit_classifier)
 
-# demo chart
-# list all data in history
-print(history.history.keys())
-# summarize history for accuracy
-plt.figure(figsize=(12,10))
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
-# summarize history for loss
-plt.figure(figsize=(12,10))
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
-
-# load model checkpoint !Important
-vit_classifier.load_weights("./tmp/checkpoint")
-
-
-# grad cam implementation
+# GRAD CAM DEF
 def get_img_array(img):
     # `array` is a float32 Numpy array of shape (299, 299, 3)
     array = keras.preprocessing.image.img_to_array(img)
@@ -343,7 +71,7 @@ def get_img_array(img):
     array = np.expand_dims(array, axis=0)
     return array
 
-# grad cam heatmap
+
 def gradcam_heatmap(img_array, model, last_conv_layer_name, pred_index=None):
     # First, we create a model that maps the input image to the activations
     # of the last conv layer as well as the output predictions
@@ -378,11 +106,10 @@ def gradcam_heatmap(img_array, model, last_conv_layer_name, pred_index=None):
     return heatmap.numpy()
 
 
-# check class lables before display
 classes.values()
 
-# display gradcam
-def display_gradcam(img, heatmap, cam_path="cam.jpg", alpha=0.4, preds=[0, 0, 0, 0, 0], plot=None):
+
+def display_gradcam(img, heatmap, cam_path="cam.jpg", alpha=0.4, preds=[0, 0, 0, 0, 0]):
     # Rescale heatmap to a range 0-255
     heatmap = np.uint8(255 * heatmap)
 
@@ -404,59 +131,54 @@ def display_gradcam(img, heatmap, cam_path="cam.jpg", alpha=0.4, preds=[0, 0, 0,
     superimposed_img = keras.preprocessing.image.array_to_img(superimposed_img)
 
     # Save the superimposed image
-    # superimposed_img.save(cam_path)
+    superimposed_img.save(cam_path)
 
     # Display Grad CAM
-    plot.imshow(superimposed_img)
-    plot.set(title=
-             " No DR: \
-             {:.3f}\nMild: \
-             {:.3f}\nModerate: \
-             {:.3f}\nSevere: \
-             {:.3f}\nProliferative: \
-             {:.3f}".format(preds[0], \
-                            preds[1], \
-                            preds[2], \
-                            preds[3],
-                            preds[4])
-             )
-    plot.axis('off')
 
 
-# grad heatmap on test image
-test_image = next(iter(test_loader))[0][5]
-# Prepare image
-img_array =get_img_array(test_image)
+#     plot.imshow(superimposed_img)
+#     plot.set(title =
+#         " No DR: \
+#         {:.3f}\nMild: \
+#         {:.3f}\nModerate: \
+#         {:.3f}\nSevere: \
+#         {:.3f}\nProliferative: \
+#         {:.3f}".format(preds[0], \
+#                     preds[1], \
+#                     preds[2], \
+#                     preds[3],
+#                     preds[4])
+#     )
+#     plot.axis('off')
 
-last_conv_layer_name = 'layer_normalization'
+#  test single image from file
+# Import required libraries
+import tensorflow as tf
+from PIL import Image
+
+# Read a PIL image
+img = Image.open(sample_image_path)
+# print(img)
+# Convert the PIL image to Tensor
+img_to_tensor = tf.convert_to_tensor(img)
+# print the converted Torch tensor
+print(img_to_tensor)
+# print("dtype of tensor:",img_to_tensor.dtype)
+
+
+# pridict image
+img_array = get_img_array(img_to_tensor)
 # Remove last layer's softmax
 vit_classifier.layers[-1].activation = None
+
+# check the model to ensure the layer name matches
+last_conv_layer_name = 'layer_normalization_4'
+
 # Print what the top predicted class is
 preds = vit_classifier.predict(img_array)
-print("Predicted:\n" + "No DR: \
-    {p1}\nMild: {p2}\nModerate: \
-    {p3}\nSevere: \
-    {p4}\nProliferative: {p5}".format(p1=preds[0][0], \
-                                            p2=preds[0][1],p3=preds[0][2],p4=preds[0][3],p5=preds[0][4]))
-# Generate class activation heatmap
 heatmap = gradcam_heatmap(img_array, vit_classifier, last_conv_layer_name)
-heatmap = np.reshape(heatmap, (36,36))
-# Display heatmap
-plt.matshow(heatmap)
 
-plt.show()
+heatmap = np.reshape(heatmap, (36, 36))
+display_gradcam(img_to_tensor, heatmap, preds=preds[0])
 
-
-
-# show actual image
-fig, axis = plt.subplots(3, 2, figsize=(30, 30))
-for images, ax in zip(next(iter(test_loader))[0][:6], axis.flat):
-    img_array = get_img_array(images)
-    # Remove last layer's softmax
-    vit_classifier.layers[-1].activation = None
-    # Print what the top predicted class is
-    preds = vit_classifier.predict(img_array)
-    heatmap = gradcam_heatmap(img_array, vit_classifier, last_conv_layer_name)
-
-    heatmap = np.reshape(heatmap, (36,36))
-    display_gradcam(images, heatmap, preds=preds[0], plot=ax)
+print(preds)
